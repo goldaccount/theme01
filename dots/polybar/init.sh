49a9d3b65a1b2e3b0898c30e261dd934b97011c7
goldaccount <gold@ccou.nt>
2022-04-10 21:57:40 +0700

    Modified colors & ncmpcpp

--- dots/polybar/init.sh
+++ dots/polybar/init.sh
@@ -1,14 +1,12 @@
 #!/bin/zsh
-polybar -r -c .config/polybar/config.ini wm &; disown
-polybar -r -c .config/polybar/config.ini menu &; disown
-polybar -r -c .config/polybar/config.ini music &; disown
-polybar -r -c .config/polybar/config.ini info &; disown
-polybar -r -c .config/polybar/config.ini clock &; disown
-polybar -r -c .config/polybar/config.ini fill1 &; disown
-polybar -r -c .config/polybar/config.ini fill2 &; disown
-polybar -r -c .config/polybar/config.ini fill3 &; disown
-polybar -r -c .config/polybar/config.ini fill4 &; disown
+/usr/local/bin/polybar -r -c .config/polybar/config.ini wm &
+/usr/local/bin/polybar -r -c .config/polybar/config.ini menu &
+polybar -r -c .config/polybar/config.ini music &
+polybar -r -c .config/polybar/config.ini info &
+polybar -r -c .config/polybar/config.ini clock &
+polybar -r -c .config/polybar/config.ini fill1 &
+polybar -r -c .config/polybar/config.ini fill2 &
+polybar -r -c .config/polybar/config.ini fill3 &
+polybar -r -c .config/polybar/config.ini fill4 &
 #polybar -r -c .config/polybar/config.ini fill5 &; disown
 #polybar -r -c .config/polybar/config.ini fill3 &; disown
-
-
