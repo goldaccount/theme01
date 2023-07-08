974b816d27ea46e3da677b5434ff028572485d52
nr200 <nr200.home>
2023-07-08 18:39:00 +0700

    Rename discord link remove tampermonkey script

--- dots/firefoxaddon/tamperdiscord.js
+++ dots/firefoxaddon/tamperdiscord.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Discord links
+// @name         Discord links remove
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
