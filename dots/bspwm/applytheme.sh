34cc1487b77084c1eda4503440c24118de8404bb
nr200 <nr200.home>
2023-07-26 06:52:50 +0700

    Rename colors file names for use with theme.sh Add Chloe color

--- dots/bspwm/applytheme.sh
+++ dots/bspwm/applytheme.sh
@@ -1,12 +1,5 @@
 #!/bin/zsh
-#. $HOME/.config/colors/color_aqua
-#. $HOME/.config/colors/color_polka
-#. $HOME/.config/colors/color_miku
-#. $HOME/.config/colors/color_hpe2
-#. $HOME/.config/colors/color_azki
-. $HOME/.config/colors/color_azki22
-#. $HOME/.config/colors/color_hachi
-#. $HOME/.config/colors/color_rushia
+. $HOME/.config/colors/color_Chloe
 
 bspc config normal_border_color $inactiveborder
 bspc config focused_border_color $activeborder
